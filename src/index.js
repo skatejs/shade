63339ed434b66df9780edc3e6c580121b54f7af5
Trey Shugart <treshugart@gmail.com>
2015-05-31 21:14:36 +1000

    Bind attr binding.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import apiBind from './api/bind';
 import apiBindings from './api/bindings';
 import apiListen from './api/listen';
 import apiNotify from './api/notify';
+import bindingAttr from './binding/attr';
 import bindingChecked from './binding/checked';
 import bindingContent from './binding/content';
+import bindingIf from './binding/if';
 import bindingName from './binding/name';
 import bindingText from './binding/text';
+import fragmentFromCollection from './util/fragment-from-collection';
 import fragmentFromString from './util/fragment-from-string';
 
-function shade () {
+function create () {
   function define (tmpHtml = '') {
     tmpHtml = tmpHtml.toString().trim();
     return function (el) {
@@ -18,7 +21,7 @@ function shade () {
         el = fragmentFromString(el).children[0];
       }
 
-      initialContent = fragmentFromString(el.innerHTML);
+      initialContent = fragmentFromCollection(el.childNodes);
       el.innerHTML = tmpHtml;
 
       apiBindings.forEach(function (binding) {
@@ -34,12 +37,16 @@ function shade () {
   define.listen = apiListen;
   define.notify = apiNotify;
 
+  define.bind('[attr]', bindingAttr);
   define.bind('input[name][type="checkbox"]', bindingChecked);
   define.bind('content, [content]', bindingContent);
+  define.bind('[if]', bindingIf);
   define.bind('input[name][type="text"]', bindingName);
   define.bind('[text]', bindingText);
 
   return define;
 }
 
+var shade = create();
+shade.create = create;
 export default window.shade = shade;
