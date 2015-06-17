4888ab4feb880327fbbb3002065f47b50e9f3ae5
Trey Shugart <treshugart@gmail.com>
2015-06-18 00:06:32 +1000

    Convert name binding to use the skate.property event instead of it's custom ones.

--- src/binding/name.js
+++ src/binding/name.js
@@ -1,11 +1,11 @@
-import apiListen from '../api/listen';
-import utilPropProxy from '../util/prop-proxy';
 import eventListen from '../event/listen';
 
 export default function (el, target) {
   var name = target.getAttribute('name');
-  utilPropProxy(el, name);
-  apiListen(el, name, () => target.value = el[name] || '');
+  el.addEventListener('skate.property', function (e) {
+    if (name !== e.detail.name) { return; }
+    target.value = e.detail.newValue || '';
+  });
   eventListen(el, ['change', 'keyup'], () => el[name] = target.value);
   el[name] = target.value;
 }
